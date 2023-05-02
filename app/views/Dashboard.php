e23320b3cb3ea449037b193af86f5737b303bf6b
if318004 <gitasiahaan26@gmail.com>
2023-05-02 23:40:52 +0700

    Add Masthead

--- app/views/Dashboard.php
+++ app/views/Dashboard.php
@@ -10,7 +10,6 @@
             <meta charset="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <meta name="description" content="" />
-            <meta name="author" content="" />
             <a class="navbar-brand" href="#page.com">GoJualan</a>
             <!-- Favicon-->
             <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
