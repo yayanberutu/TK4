6923545be37d9c0dc1c11bd8c3c0e00e1ce5c328
yayanberutu <yoseprib@gmail.com>
2023-05-04 13:42:29 +0700

    initialize file config and add config for database and BASEURL

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * File ini merupakan file tempat mendefenisikan config config yang diperlukan
+ */
+
+define('BASEURL', 'http://localhost:8080/yayanberutu/TK4/public');
+
+/**
+ * Config dibawah merupakan config database lokal.
+ * Silahkan sesuaikan dengan config masing-masing!
+ */
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_tokoku');
