7d697faad8b0792f26ef706d494a0119e65e2984
yayanberutu <yoseprib@gmail.com>
2023-05-04 15:21:47 +0700

    change pattern of request url

--- app/core/App.php
+++ app/core/App.php
@@ -10,12 +10,12 @@ class App {
         $url = $this->parseURL();
         
         // controller
-        if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
+        if( file_exists('../app/controllers/' . $url[0] . 'Controller.php') ) {
             $this->controller = $url[0];
             unset($url[0]);
         }
 
-        require_once '../app/controllers/' . $this->controller . '.php';
+        require_once '../app/controllers/' . $this->controller . 'Controller.php';
         $this->controller = new $this->controller;
 
         // method
