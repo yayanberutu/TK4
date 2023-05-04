31d37dbdb115de6cdf9cc414d51aa354ce3d3c09
yayanberutu <yoseprib@gmail.com>
2023-05-04 13:50:48 +0700

    implement BaseController class

---
+++ app/core/BaseController.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * This class is the base controller of this projects
+ */
+class BaseController {
+    public function view($view, $data = [])
+    {
+        require_once '../app/views/' . $view . '.php';
+    }
+
+    public function model($model)
+    {
+        require_once '../app/models/' . $model . '.php';
+        return new $model;
+    }
+}
