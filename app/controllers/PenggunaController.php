a01a3c183c511cb870894051c18de8ecea6de991
yayanberutu <yoseprib@gmail.com>
2023-05-04 19:39:30 +0700

    remove unused echo

--- app/controllers/PenggunaController.php
+++ app/controllers/PenggunaController.php
@@ -5,7 +5,6 @@ class PenggunaController extends BaseController{
     public function index(){
         $data['title'] = 'Halaman Pengguna';
         $data['users'] = $this->model('PenggunaModel')->getAll();
-        echo $data['users'];
         $this->view("templates/header", $data);
         $this->view("templates/sidebar", $data);
         $this->view("/admin/pengguna", $data);
