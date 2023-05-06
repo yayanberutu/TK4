91e7310459d998fd808fd2e5dfac1effc1eb254c
yayanberutu <yoseprib@gmail.com>
2023-05-06 21:47:44 +0700

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -2,14 +2,14 @@
 /**
  * File ini merupakan file tempat mendefenisikan config config yang diperlukan
  */
-
-define('BASEURL', 'http://localhost:8080/yayanberutu/TK4/public');
+$base_url  = 'https://' . $_SERVER['HTTP_HOST'];
+define('BASEURL', $base_url);
 
 /**
  * Config dibawah merupakan config database lokal.
  * Silahkan sesuaikan dengan config masing-masing!
  */
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'db_inventaris');
+define('DB_HOST', 'parkoding.tech');
+define('DB_USER', 'u1572699_admin');
+define('DB_PASS', 'AmsAl1:7A!');
+define('DB_NAME', 'u1572699_db_inventory');
