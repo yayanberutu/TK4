0dae6a188e0ee254cecc6fb55fb19208730353d8
if318004 <gitasiahaan26@gmail.com>
2023-05-06 18:21:08 +0700

    able to show keuntungan chart

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,4 +1,4 @@
-  <!-- Content Wrapper. Contains page content -->
+<!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -20,7 +20,41 @@
           <h3 class="card-title">Hello</h3>
         </div>
         <div class="card-body">
-          Selamat datang dihalaman admin!
+          
+
+        <div class="card bg-gradient-info">
+              <div class="card-header border-0">
+                <h3 class="card-title">
+                  <i class="fas fa-th mr-1"></i>
+                  Sales Graph
+                </h3>
+                <div class="card-tools">
+                  <button type="button" class="btn bg-info btn-sm" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+              
+
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer bg-transparent">
+                <div class="row">
+                  <!-- ./col -->
+                  
+                  <!-- ./col -->
+                </div>
+                <!-- /.row -->
+              </div>
+              <!-- /.card-footer -->
+            </div>
+
+
         </div>
         <!-- /.card-body -->
         <div class="card-footer">
@@ -35,3 +69,50 @@
   </div>
   <!-- /.content-wrapper -->
 
+
+  <!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- ChartJS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+
+<script>
+// Panggil fungsi untuk mengambil data penjualan/keuntungan
+$.ajax({
+  url: "<?= BASEURL; ?>/<?= $data['controllerName'] ?>/getDataKeuntungan",
+  type: 'POST',
+  success: function(result) {
+    // Parse result to a JavaScript object
+    var dataObj = JSON.parse(result);
+
+    // Olah data hasil query
+    var labels = [];
+    var data = [];
+    dataObj.forEach(function(row) {
+      labels.push(row.TanggalPenjualan);
+      data.push(row.Keuntungan);
+    });
+
+    var ctx = document.getElementById('line-chart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: data,
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+});
+
+</script>
