0145e3aa6f922ec0bcfef080884c8f9871e2d7ec
if318004 <gitasiahaan26@gmail.com>
2023-05-07 02:00:29 +0700

    AdminModel.php

--- app/models/AdminModel.php
+++ app/models/AdminModel.php
@@ -9,7 +9,22 @@ class AdminModel extends BaseModel {
 
         $this->db->query("SELECT TanggalPenjualan, SUM(JumlahPenjualan * HargaJual) AS Keuntungan
                         FROM Penjualan
-                        GROUP BY TanggalPenjualan");
+                        WHERE TanggalPenjualan >= DATE_SUB(NOW(), INTERVAL 7 DAY)
+                        GROUP BY TanggalPenjualan
+                        ORDER BY TanggalPenjualan
+                        ");
+        
+        $result = $this->db->resultSet();
+
+        return $result;
+    }
+
+    public function getAllDataKerugianPenjualanView(){
+
+        $this->db->query("SELECT TanggalPembelian, SUM(JumlahPembelian * HargaBeli) AS Kerugian
+                        FROM Pembelian 
+                        WHERE TanggalPembelian >= DATE_SUB(NOW(), INTERVAL 7 DAY) 
+                        GROUP BY TanggalPembelian");
         
         $result = $this->db->resultSet();
 
