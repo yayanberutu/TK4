376162a54d73df950b469e1ebb4d394789213f06
yayanberutu <yoseprib@gmail.com>
2023-05-28 00:07:46 +0700

    temporary comment

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php 
-if( !session_id() ) session_start();
-require_once '../app/core/App.php';
-require_once '../app/core/BaseController.php';
-require_once '../app/models/BaseModel.php';
-require_once '../app/core/Database.php';
-require_once '../app/core/Flasher.php';
+// if( !session_id() ) session_start();
+// require_once '../app/core/App.php';
+// require_once '../app/core/BaseController.php';
+// require_once '../app/models/BaseModel.php';
+// require_once '../app/core/Database.php';
+// require_once '../app/core/Flasher.php';
 
-require_once '../app/config/config.php';
+// require_once '../app/config/config.php';
 
 // $app = new App;
 //coba push ke niagahoster
