60d1a47bb50fb21c76a4d75974bda7cc60ce7be0
yayanberutu <yoseprib@gmail.com>
2023-05-28 12:55:25 +0700

    resolve css path

--- app/views/Dashboard.php
+++ app/views/Dashboard.php
@@ -21,7 +21,7 @@
             <!-- SimpleLightbox plugin CSS-->
             <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
             <!-- Core theme CSS (includes Bootstrap)-->
-            <link rel="stylesheet" type="text/css" href="<?= PROJECT_URL ?>/public/css/Style.css">
+            <link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/public/css/Style.css">
         </div>
     </nav>
     <!-- Masthead-->
